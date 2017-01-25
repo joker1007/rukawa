1b5122c77e1930d9d2daff6d611be6d7046f22f0
joker1007 <kakyoin.hierophant@gmail.com>
2017-01-25 15:08:43 +0900

    Add preview param to Builtins::Embulk

--- lib/rukawa/builtins/embulk.rb
+++ lib/rukawa/builtins/embulk.rb
@@ -3,15 +3,16 @@ require 'rukawa/builtins/shell'
 module Rukawa
   module Builtins
     class Embulk < Shell
-      class_attribute :config, :embulk_bin, :embulk_bundle, :embulk_vm_options, :jvm_options
+      class_attribute :config, :embulk_bin, :embulk_bundle, :embulk_vm_options, :jvm_options, :preview
 
       self.embulk_bin = "embulk"
       self.embulk_bundle = nil
       self.embulk_vm_options = []
       self.jvm_options = []
+      self.preview = false
 
       class << self
-        def handle_parameters(config:, embulk_bin: nil, embulk_bundle: nil, embulk_vm_options: nil, jvm_options: nil, stdout: nil, stderr: nil, env: nil, chdir: nil)
+        def handle_parameters(config:, embulk_bin: nil, embulk_bundle: nil, embulk_vm_options: nil, jvm_options: nil, stdout: nil, stderr: nil, env: nil, chdir: nil, preview: false)
           self.config = config
           self.embulk_bin = embulk_bin if embulk_bin
           self.embulk_bundle = embulk_bundle if embulk_bundle
@@ -21,13 +22,14 @@ module Rukawa
           self.stderr = stderr if stderr
           self.env = env if env
           self.chdir = chdir if chdir
+          self.preview = preview
         end
       end
 
       def run
         process = -> do
-          if ENV["EMBULK_DRY_RUN"]
-            stdout.puts File.read(config)
+          if preview
+            stdout.puts "Config:\n#{File.read(config)}"
             cmds = [embulk_bin, "preview", *embulk_bundle, config].compact
             stdout.puts cmds.join(" ")
           else
